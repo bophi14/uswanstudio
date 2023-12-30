5b25e58094950ad4766a193bfdb6dfd95384fe2a
Bophi <fm.iqbal14@gmail.com>
2023-12-31 01:12:36 +0700

    uswan studio

---
+++ js/script.js
@@ -0,0 +1,72 @@
+/*==================== toggle icon navbar ====================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+function whatsapp(){
+    var name = document.getElementById('name').value;
+    var message = document.getElementById('message').value;
+    var email = document.getElementById('email').value;
+    var nomor = document.getElementById('number').value;
+
+    var url = "https://wa.me/628871991081?text=Halo%2C%20Saya%20" + name + "%0a" + "Email: " + email + "%0a" + "Nomor HP: " + nomor + "%0a" + message;
+
+    window.open(url, '_blank').focus();
+}
+/*==================== scroll sections active link ====================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    /*==================== sticky navbar ====================*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*==================== remove toggle icon and navbar when click navbar link (scroll) ====================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
+/*==================== scroll reveal ====================*/
+ScrollReveal({
+    reset: true, //jadiin comment kalo gamau terus animasi
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+/*==================== typed js ====================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Voice Over Artist', 'Singer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
