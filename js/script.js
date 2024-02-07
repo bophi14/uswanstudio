2d49b3f60886445977ebf590a822c9e1f87c6fa0
Bophi <fm.iqbal14@gmail.com>
2024-02-08 05:59:07 +0700

    nambah loading

--- js/script.js
+++ js/script.js
@@ -94,7 +94,7 @@ jQuery(function ($) {
         // initialize playlist and controls
         var index = 0,
             playing = false,
-            mediaPath = 'https://archive.org/download/portfolio_yuswantina/',
+            mediaPath = '/audios/',
             extension = '',
             tracks = [{
                 "track": 1,
